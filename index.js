0d68dfd4e52aa9ffb91cb1febedab653fe9cbcb9
adarshtfs <adarsh.raj@taxiforsure.com>
2015-07-29 11:51:48 +0530

    just query generation part

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 var MongoClient = require('mongodb').MongoClient,
-    logger = require("simplehelper").logger.getInstance();
+    logger = require("simplehelper").logger.getInstance(),
+    fs = require('fs');
 
 var replicaDB_Con = undefined;
 var COLLECTION_DEVICEDETAILS = 'device_details';
+var settings = JSON.parse(fs.readFileSync(__dirname + "/settings.json", 'utf8'));
+var connectionString = settings.REPLICA_SET_CONN_URL_SLAVE_READ;
+
+MongoClient.connect(connectionString, function(err, dbc) {
+    if(err) throw err;
+    replicaDB_Con= dbc;
+});
+
+
 
 /*
 
@@ -19,91 +29,31 @@ var COLLECTION_DEVICEDETAILS = 'device_details';
 
 var nearest_driver = {
 
-    initConnection : function(connectionString,calback){
-
-        MongoClient.connect(connectionString, function(err, dbc) {
-            if(err) throw err;
-            callback(dbc);
-        });
-    },
-    getNearestDrivers : function(type,city,Condition,cartypes,options){
+    generateQuery : function(type,city,conditions) {
 
 
-        var time_in_seconds_to_mark_signal_loss = options.time_in_seconds_to_mark_signal_loss;
-        var login_cut_off_time = options.login_cut_off_time;
-        var meter_off_cooltime_seconds = options.meter_off_cooltime_seconds;
+        var time_in_seconds_to_mark_signal_loss = settings.time_in_seconds_to_mark_signal_loss;
+        var login_cut_off_time = settings.login_cut_off_time;
+        var meter_off_cooltime_seconds = settings.meter_off_cooltime_seconds;
 
-        var poll_time_cutoff =  parseInt(new Date().getTime()/1000, 10) - time_in_seconds_to_mark_signal_loss;
+        var poll_time_cutoff = parseInt(new Date().getTime() / 1000, 10) - time_in_seconds_to_mark_signal_loss;
         // Have to filter out the devices where login time is less than 5 minutes
         var today = new Date();
         var login_time_cutoff = new Date(today);
         login_time_cutoff.setMinutes(today.getMinutes() - login_cut_off_time);
-        var meteoff_coolof_time=parseInt(new Date().getTime(), 10) - meter_off_cooltime_seconds*1000;
+        var meteoff_coolof_time = parseInt(new Date().getTime(), 10) - meter_off_cooltime_seconds * 1000;
 
         var query = {
             state: "Free",
             isBlocked: "false",
             isOperatorActive: true,
             isActive: true,
-            updatedAt : {$gt: poll_time_cutoff.toString()},
-            loggedInEpochTime: { $lt: login_time_cutoff.getTime()},
-            city : city
+            updatedAt: {$gt: poll_time_cutoff.toString()},
+            loggedInEpochTime: {$lt: login_time_cutoff.getTime()}
+            /*,meterOffTime: {$lt:meteoff_coolof_time.toString()}*/
         };
 
-        /*meterOffTime: {$lt:meteoff_coolof_time.toString()}*/
-
-        if(type === 'api_server'){
-
-
-            query['$or']=conditions;
-
-            var projection  = { "latitude": 1, "longitude": 1, "meterOffTime": 1, "dispatchPreference": 1, "blackmarkedDriver": 1, "carType": 1 };
-
-
-        }else if(type === 'consumer_app'){
-
-           var projection = { "latitude": 1, "longitude": 1, "meterOffTime": 1, "city": 1, "uuid": 1, "carType": 1, "carKind": 1 };
-        }
-
-        try {
-
-            if(replicaDB_Con !== undefined){
-
-                replicaDB_Con.collection(COLLECTION_DEVICEDETAILS).find(query, projection).toArray(function (err, drivers) {
-                    if (err) {
-                        callback(err, null);
-                    } else {
-                        callback(null, drivers);
-                    }
-                });
-
-            }else{
-
-                nearest_driver.initConnection(String,function(con){
-
-                    replicaDB_Con=con
-
-                    replicaDB_Con.collection(COLLECTION_DEVICEDETAILS).find(query, projection).toArray(function (err, drivers) {
-                        if (err) {
-                            callback(err, null);
-                        } else {
-                            callback(null, drivers);
-
-                        }
-                    });
-
-                });
-            }
-        }
-        catch(e){
-
-            logger.error("device_details/getNearestDrivers","" +query+ " Error: "+e);
-            callback();
-        }
     }
 
-
 }
-
-
 module.exports = nearest_driver;
